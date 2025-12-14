f2738ef511aebf0e11fa95c0bc5d07850b16ec4b
Daniel Regeci <536331+ovx@users.noreply.github.com>
2025-12-14 15:53:53 +0700

    v1.0.0

--- mix.exs
+++ mix.exs
@@ -4,9 +4,9 @@ defmodule Altcha.MixProject do
   def project do
     [
       app: :altcha,
-      version: "0.2.0",
+      version: "1.0.0",
       elixir: "~> 1.17",
-      build_embedded: Mix.env == :prod,
+      build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
       description: description(),
       package: package(),
@@ -24,7 +24,10 @@ defmodule Altcha.MixProject do
     [
       name: "altcha",
       licenses: ["MIT"],
-      links: %{"GitHub" => "https://github.com/altcha-org/altcha-lib-ex", "Web" => "https://altcha.org"}
+      links: %{
+        "GitHub" => "https://github.com/altcha-org/altcha-lib-ex",
+        "Web" => "https://altcha.org"
+      }
     ]
   end
 
@@ -39,7 +42,7 @@ defmodule Altcha.MixProject do
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
       {:jason, "~> 1.4"},
-      {:ex_doc, "~> 0.14", only: :dev, runtime: false}
+      {:ex_doc, "~> 0.39", only: :dev, runtime: false}
     ]
   end
 end
