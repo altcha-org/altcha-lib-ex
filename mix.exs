7dcb35f110f980ab3b2a640cec3566728ed360bb
Daniel Regeci <536331+ovx@users.noreply.github.com>
2026-04-07 19:15:18 +0200

    v2.0.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Altcha.MixProject do
   def project do
     [
       app: :altcha,
-      version: "2.0.0-beta.1",
+      version: "2.0.0",
       elixir: "~> 1.17",
       build_embedded: Mix.env() == :prod,
       start_permanent: Mix.env() == :prod,
